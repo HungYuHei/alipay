a9aa1e41aaf32f97cde01579bc028b61ab27fc1b
H.J.LeoChen <hjleochen@hotmail.com>
2014-03-21 16:12:19 +0800

    wap notify verify

--- lib/alipay/notify.rb
+++ lib/alipay/notify.rb
@@ -1,9 +1,20 @@
 module Alipay
   module Notify
     module Wap
+      #for call_back_url return
+      def self.verify_sign_only?(params)
+        params = Utils.stringify_keys(params)
+        Sign::Wap.verify?(params)
+      end
+
+      #for notify_url return
       def self.verify?(params)
         params = Utils.stringify_keys(params)
-        Sign::Wap.verify?(params) && Notify.verify_notify_id?(params['notify_id'])
+
+        data = params['notify_data']
+        notify_id = $~[1] if data =~ /<notify_id>(.*?)<\/notify_id>/
+
+        Sign::Wap.verify?(params) && Notify.verify_notify_id?(notify_id)
       end
     end
 
